b86caaf2db2202cbcf3d9452226ed1e168335918
Brunno Vodola <bvodola@gmail.com>
2020-07-06 15:20:04 -0300

    Updating babel packages

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,53 +1,73 @@
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './public/index.html',
-  filename: 'index.html',
-  inject: 'body'
-})
+  template: "./public/index.html",
+  filename: "index.html",
+  inject: "body",
+});
 
 module.exports = {
-  mode: 'development',
-  devtool: 'eval-source-map',
-  entry: [
-    'react-hot-loader/patch',
-    './src/index.js'
-  ],
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: ["react-hot-loader/patch", "./src/index.js"],
   output: {
-    path: path.resolve('build'),
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: path.resolve("build"),
+    publicPath: "/",
+    filename: "bundle.js",
   },
   module: {
     rules: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.sass$/, use: [{loader: "style-loader" }, {loader: "css-loader"}, {loader: "sass-loader"}], exclude: /node_modules/},
-      { test: /\.scss$/, use: [{loader: "style-loader" }, {loader: "css-loader"}, {loader: "sass-loader"}], exclude: /node_modules/}
-    ]
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env", "@babel/preset-react"] },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx$/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env", "@babel/preset-react"] },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
   },
   devServer: {
     port: 4000,
     hot: true,
     historyApiFallback: true,
-    proxy: {
-      "/api/**": {
-        target: "http://localhost:2000",
-        secure: false,
-        pathRewrite: { "$/": "" }
-      },
-    }
   },
   plugins: [
     HtmlWebpackPluginConfig,
-    new webpack.NamedModulesPlugin(), 
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ],
   resolve: {
     alias: {
-      src: path.resolve(__dirname, '../src')
-    }
-  }
-}
+      src: path.resolve(__dirname, "../src"),
+    },
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import 'babel-polyfill';
-import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader'
-import App from './App';
+import React from "react";
+import "@babel/polyfill";
+import ReactDOM from "react-dom";
+import { AppContainer } from "react-hot-loader";
+import App from "./App";
 
-const render = Component => {
+const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
       <Component />
     </AppContainer>,
-    document.getElementById('root')
-  )
-}
+    document.getElementById("root")
+  );
+};
 
 render(App);
 if (module.hot) {
-  module.hot.accept('./App', () => { render(App) })
+  module.hot.accept("./App", () => {
+    render(App);
+  });
 }
