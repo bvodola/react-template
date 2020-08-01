1759e919e4edb825139b43dac5a9e653a4e9a798
Brunno Vodola <bvodola@gmail.com>
2020-08-01 00:18:44 -0300

    Updating webpack production config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,39 +1,76 @@
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './public/index.html',
-  filename: 'index.html',
-  inject: 'body'
-})
+  template: "./public/index.html",
+  filename: "index.html",
+  inject: "body",
+});
 
 module.exports = {
-  devtool: 'cheap-module-source-map',
-  entry: [
-    './src/index.js'
-  ],
+  mode: "production",
+  devtool: "cheap-module-source-map",
+  entry: ["./src/index.js"],
   output: {
-    path: path.resolve('build'),
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: path.resolve("build"),
+    publicPath: "/",
+    filename: "bundle.js",
   },
   module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env", "@babel/preset-react"] },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx$/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env", "@babel/preset-react"] },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
   },
   plugins: [
     HtmlWebpackPluginConfig,
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false,
-      mangle: false
-    }),
+    new CopyWebpackPlugin([{ from: "public", to: "public" }]),
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
-  ]
-}
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "../src"),
+    },
+  },
+};
